0fdf58b206bb5efe9aa89444b4ffca64ca251edd
Teddy <tedrandby@gmail.com>
2020-09-21 10:11:01 -0400

    fixed folder name

--- src/components/logic/LogicSearchBar.ts
+++ src/components/logic/LogicSearchBar.ts
@@ -1,25 +1,35 @@
-import { useReducer, } from 'react'
-import { DesignSearchBar } from "../ design/DesignSearchBar"
+import { useReducer } from "react";
+import { DesignSearchBar } from "../ design/DesignSearchBar";
 
 interface QueryState {
-    queryType: string
+  queryType: string;
 }
 
 export const LogicSearchBar = () => {
-    let PREDATORS = 'predators';
-    let PREY = 'prey';
-    let EITHER = 'predators and prey';
+  let PREDATORS = "predators";
+  let PREY = "prey";
+  let EITHER = "predators and prey";
+  // TODO:
+  //  -
 
-    const QueryStateReducer = (state: QueryState): QueryState => {
-        switch (state.queryType) {
-            case PREDATORS: return { queryType: PREY };
-            case PREY: return { queryType: EITHER };
-            case EITHER: return { queryType: PREDATORS };
-            default: return state;
-        }
+  const QueryStateReducer = (state: QueryState): QueryState => {
+    switch (state.queryType) {
+      case PREDATORS:
+        return { queryType: PREY };
+      case PREY:
+        return { queryType: EITHER };
+      case EITHER:
+        return { queryType: PREDATORS };
+      default:
+        return state;
     }
+  };
 
-    const [queryState, dispatchQueryState] = useReducer(QueryStateReducer, { queryType: PREDATORS })
-    return DesignSearchBar({ queryState: queryState.queryType, dispatchQueryState: dispatchQueryState })
-
-}
+  const [queryState, dispatchQueryState] = useReducer(QueryStateReducer, {
+    queryType: PREDATORS,
+  });
+  return DesignSearchBar({
+    queryState: queryState.queryType,
+    dispatchQueryState: dispatchQueryState,
+  });
+};
