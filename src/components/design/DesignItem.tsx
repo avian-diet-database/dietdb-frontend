fa0ee6160fdc93ae0f979f1e134ddd8c4d4d5e51
Teddy <tedrandby@gmail.com>
2020-09-25 15:11:00 -0400

    New files

---
+++ src/components/design/DesignItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface DesignItemProps {
+    itemData: any
+}
+
+export const DesignItem = (props: DesignItemProps) => {
+    return (
+        <div>Hello world</div>
+    )
+}
---
+++ src/components/logic/LogicItem.ts
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+interface LogicItemProps {
+    activeItem: string
+}
+
+export const LogicItem = (props: LogicItemProps) => {
+
+
+}
