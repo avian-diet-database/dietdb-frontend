a7a94b4fee1afe922a0ea7f2d84065be55ef74ee
muyanpan <muyan@live.unc.edu>
2020-10-02 13:34:34 -0400

    trying to implement node sass to change Bulma defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { LogicSearchBar } from "./components/logic/LogicSearchBar";
-import "./css/App.css";
 import "..\\node_modules\\bulma\\css\\bulma.css";
 import { LogicHeader } from "./components/logic/LogicHeader";
 import { LogicFooter } from "./components/logic/LogicFooter";
