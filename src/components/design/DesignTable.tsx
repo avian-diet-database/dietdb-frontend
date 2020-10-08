9868fee1c6263941d0173ce2f1cbf15e8739d3be
Teddy <tedrandby@gmail.com>
2020-10-08 08:43:54 -0400

    added box to table styles

--- src/components/design/DesignTable.tsx
+++ src/components/design/DesignTable.tsx
@@ -1,44 +1,46 @@
-import React from 'react';
-import { Prey } from '../../types/Prey';
+import React from "react";
+import { Prey } from "../../types/Prey";
 
 interface DesignTableProps {
-    prey: Prey[]
+  prey: Prey[];
 }
 
 export const DesignTable = (props: DesignTableProps) => {
-    let tableBody =
-        props.prey.map(item => {
-            return <tr key={props.prey.indexOf(item)}>
-                <th>{item.taxon}</th>
-                <td>{item.items}</td>
-                <td>{item.wt_or_vol}</td>
-                <td>{item.occurence}</td>
-                <td>{item.unspecified}</td>
-            </tr>
-        });
+  let tableBody = props.prey.map((item) => {
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Taxon</th>
-                    <th>Items</th>
-                    <th>Weight or Volume</th>
-                    <th>Occurrence</th>
-                    <th>Unspecified</th>
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th>Taxon</th>
-                    <th>Items</th>
-                    <th>Weight or Volume</th>
-                    <th>Occurrence</th>
-                    <th>Unspecified</th>
-                </tr>
-            </tfoot>
-            <tbody>
-                {tableBody}
-            </tbody>
-        </table>
-    )
-}
+      <tr key={props.prey.indexOf(item)}>
+        <th>{item.taxon}</th>
+        <td>{item.items}</td>
+        <td>{item.wt_or_vol}</td>
+        <td>{item.occurence}</td>
+        <td>{item.unspecified}</td>
+      </tr>
+    );
+  });
+  return (
+    <div className="box">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Taxon</th>
+            <th>Items</th>
+            <th>Weight or Volume</th>
+            <th>Occurrence</th>
+            <th>Unspecified</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th>Taxon</th>
+            <th>Items</th>
+            <th>Weight or Volume</th>
+            <th>Occurrence</th>
+            <th>Unspecified</th>
+          </tr>
+        </tfoot>
+        <tbody>{tableBody}</tbody>
+      </table>
+    </div>
+  );
+};
+
