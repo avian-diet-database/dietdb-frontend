c3b2ebfde4210bbce17138358c2d6f8c27f3d2e8
Teddy <tedrandby@gmail.com>
2020-10-12 16:09:58 -0400

    Table more dynamic

--- src/components/design/DesignTable.tsx
+++ src/components/design/DesignTable.tsx
@@ -2,42 +2,35 @@ import React from "react";
 import { Prey } from "../../types/Prey";
 
 interface DesignTableProps {
-  prey: Prey[];
+  prey: any[];
 }
 
 export const DesignTable = (props: DesignTableProps) => {
-  let tableBody = props.prey.map((item) => {
-    return (
-      <tr key={props.prey.indexOf(item)}>
-        <td>{item.taxon}</td>
-        <td>{item.items}</td>
-        <td>{item.wt_or_vol}</td>
-        <td>{item.occurence}</td>
-        <td>{item.unspecified}</td>
-      </tr>
-    );
-  });
+  console.log(Object.keys(props.prey))
+  if (props.prey.length < 1) {
+    return <div></div>
+  }
 
   return (
     <div className="box has-background-light is-scrollable has-text-dark" >
       <table className="table is-fullwidth has-background-light has-text-dark" >
         <thead>
           <tr>
-            <th>Taxon</th>
-            <th>Items</th>
-            <th>Weight or Volume</th>
-            <th>Occurrence</th>
-            <th>Unspecified</th>
+            {Object.keys(props.prey[0]).map(key => {
+              return <th key={key}>{key}</th>
+            })}
           </tr>
         </thead>
-        <tbody>{tableBody}</tbody>
+        <tbody>{props.prey.map(item => {
+          return <tr> {Object.keys(item).map(datum => {
+            return <td key={datum}> {item[datum]}</td>
+          })}</tr>
+        })}</tbody>
         <tfoot>
           <tr>
-            <th>Taxon</th>
-            <th>Items</th>
-            <th>Weight or Volume</th>
-            <th>Occurrence</th>
-            <th>Unspecified</th>
+            {Object.keys(props.prey[0]).map(key => {
+              return <th key={key}>{key}</th>
+            })}
           </tr>
         </tfoot>
       </table>
