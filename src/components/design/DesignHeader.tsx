0e87cd6c3df802f43d55e0f1557ff5609399313b
muyanpan <muyan@live.unc.edu>
2020-11-06 14:46:37 -0500

    map is centered

--- src/components/design/DesignHeader.tsx
+++ src/components/design/DesignHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const DesignHeader = () => {
   return (
     <div className="container">
-      <p className="title is-size-1">Avian Diet Database</p>
+      <p className="title is-size-1-">Avian Diet Database</p>
       <p className="subtitle is-3">Click on a tab below to begin exploring!</p>
     </div>
   );
