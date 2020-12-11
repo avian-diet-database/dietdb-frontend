41b9632662c7b940a9690c2a6952b808481e2e50
Teddy Randby <tedrandby@gmail.com>
2020-12-11 15:27:28 -0500

    Updated selects w value

--- src/components/design/DesignDropdown.tsx
+++ src/components/design/DesignDropdown.tsx
@@ -3,13 +3,14 @@ import React from "react";
 interface DesignDropdownProps {
   criteriaOptions: string[];
   onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  val: string;
 }
 
 //Try setting the value here, instead of relying on these onChange methods.
 export const DesignDropdown = (props: DesignDropdownProps) => {
   return (
     <div className="select is-primary is-small">
-      <select onChange={props.onChange}>
+      <select value={props.val} onChange={props.onChange}>
         {props.criteriaOptions.map((option) => (
           <option key={option}>{option}</option>
         ))}
