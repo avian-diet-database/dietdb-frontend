1930a26161504aca6b8efe39ecd78e3c600f9d5b
Pich Him <pichhim@live.unc.edu>
2021-10-06 20:58:47 -0400

    Updated ApolloClient backend url again to reflect new dev-backend pod pointing to staging branch

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { cache } from "./cache";
 const client = new ApolloClient({
   cache,
   // TODO: When finalizing changes in prod, can just uncomment prod DB link (line 13)
-  uri: "http://test-dietdb-backend-git-pichhim.apps.cloudapps.unc.edu/graphql",
+  uri: "http://dev-backend-pichhim.apps.cloudapps.unc.edu/graphql",
   // uri: "https://back-end-dept-dietdatabase.cloudapps.unc.edu/graphql",
 });
 
