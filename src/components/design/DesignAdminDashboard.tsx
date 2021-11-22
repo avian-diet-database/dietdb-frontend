e62139ab03d3cd1d4b7759fc7b13d92ea4b9fa37
Eden Chou <eden.chou0606@gmail.com>
2021-11-22 12:51:08 -0500

    adding data page

--- src/components/design/DesignAdminDashboard.tsx
+++ src/components/design/DesignAdminDashboard.tsx
@@ -147,7 +147,7 @@ export const DesignAdminDashboard = (props: DesignAdminDashboardProps) => {
                                         <p style={styles.pendingTitleTitle}>{data.source.split(',')[1]}</p>
                                         <p style={styles.pendingYearTitle}>{data.source.split(',')[2]}</p>
                                         <p style={styles.pendingUserTitle}>{data.source.split(',')[4]}</p>
-                                        <p style={styles.green}>View</p>
+                                        <p style={styles.green} >View</p>
                                         
                                         </div>
                                         <hr style={styles.backgroundGreen} />
@@ -179,6 +179,21 @@ export const DesignAdminDashboard = (props: DesignAdminDashboardProps) => {
                             className={'back-dashboard-button-pg-2'} />
                     </div>
                 </div>
+                {/* <div id="page2" style={styles.adminContainerPg2}>
+                    <div>
+                        <div style={{...styles.backgroundGreen, ...styles.tableHeader, ...styles.alignTextCenter}}>
+                            <p style={styles.pendingTableTitle}><strong style={styles.white}>Pending Approval</strong></p>
+                        </div>
+                        <div style={styles.pendingTableContent}>
+                            <p>table stuff here</p>
+                        </div>
+                    </div>
+                    <div style={styles.singleButton} onClick={() => movePgToPg('2', '1')}>
+                        <DesignLargeGreenButton 
+                            buttonText={'Back to Dashboard'} 
+                            className={'back-dashboard-button-pg-2'} />
+                    </div>
+                </div> */}
                 <div id="page3" style={styles.adminContainerPg3}>
                     <div>
                         <div style={{...styles.backgroundGreen, ...styles.tableHeader, ...styles.alignTextCenter}}>
