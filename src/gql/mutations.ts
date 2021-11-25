e24355335694d1b3a2d7ccaf6791c74145173b6c
Pich Him <pichhim@live.unc.edu>
2021-11-25 15:40:39 -0500

    Update getPendingDiet, getApprovalHistory, createPendingDiet, approvePendingDiet, and denyPendingDiet mutations and queries

--- src/gql/mutations.ts
+++ src/gql/mutations.ts
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+/* AuthenticationResolver mutations */
 const CREATE_USER = gql `
 mutation createUser($full_name: String!,
     $username: String!, $email: String!, $password: String!, 
@@ -17,6 +18,8 @@ mutation resetPassword($email: String!, $password: String!) {
         resetPassword(email: $email, password: $password)
     }
 `;
+
+/* PendingPageResolver mutations and queries */
 
 // Using this one for now
 const CREATE_PENDING_DIET_SKELETON = gql`
@@ -85,6 +88,8 @@ mutation CreatePendingDiet(
     $lastname_author: string
     $year: number
     $journal: string
+    $total_percent_diet: number
+    $new_species: boolean
     ) {
     createPendingDiet(           
         common_name: $common_name
@@ -138,66 +143,245 @@ mutation CreatePendingDiet(
         lastname_author: $string
         year: $number
         journal: $string
+        total_percent_diet: $number
+        new_species: $boolean
     )
-    {
-        id
-        common_name
-        scientific_name
-        subspecies
-        family
-        taxonomy
-        longitude_dd
-        latitude_dd
-        altitude_min_m
-        altitude_max_m
-        altitude_mean_m
-        location_region
-        location_specific
-        habitat_type
-        observation_month_begin
-        observation_month_end
-        observation_year_begin
-        observation_year_end
-        observation_season
-        analysis_number
-        prey_kingdom
-        prey_phylum
-        prey_class
-        prey_order
-        prey_suborder
-        prey_family
-        prey_genus
-        prey_scientific_name
-        inclusive_prey_taxon
-        prey_name_ITIS_ID
-        prey_name_status
-        prey_stage
-        prey_part
-        prey_common_name
-        fraction_diet
-        diet_type
-        item_sample_size
-        bird_sample_size
-        sites
-        study_type
-        notes
-        entered_by
-        source
-        doi
-        sex
-        age_class
-        within_study_data_source
-        table_fig_number
-        title
-        lastname_author
-        year
-        journal
-    }
+}
+`;
+
+const APPROVE_PENDING_DIET = gql`
+mutation ApprovePendingDiet(
+    $common_name: string!
+    $scientific_name: string
+    $subspecies: string
+    $family: string
+    $taxonomy: string
+    $longitude_dd: string
+    $latitude_dd: string
+    $altitude_min_m: string
+    $altitude_max_m: string
+    $altitude_mean_m: string
+    $location_region: string!
+    $location_specific: string
+    $habitat_type: string
+    $observation_month_begin: number
+    $observation_month_end: number
+    $observation_year_begin: number
+    $observation_year_end: number
+    $observation_season: string
+    $analysis_number: string
+    $prey_kingdom: string!
+    $prey_phylum: string
+    $prey_class: string
+    $prey_order: string
+    $prey_suborder: string
+    $prey_family: string
+    $prey_genus: string
+    $prey_scientific_name: string
+    $inclusive_prey_taxon: string
+    $prey_name_ITIS_ID: string
+    $prey_name_status: string
+    $prey_stage: string
+    $prey_part: string
+    $prey_common_name: string
+    $fraction_diet: string
+    $diet_type: string!
+    $item_sample_size: number
+    $bird_sample_size: number
+    $sites: string
+    $study_type: string
+    $notes: string
+    $entered_by: string
+    $source: string!
+    $doi: string
+    $sex: string
+    $age_class: string
+    $within_study_data_source: string
+    $table_fig_number: string
+    $title: string
+    $lastname_author: string
+    $year: number
+    $journal: string
+    $total_percent_diet: number
+    $unique_id: number
+    ) {
+    approvePendingDiet(           
+        common_name: $common_name
+        scientific_name: $scientific_name
+        subspecies: $subspecies
+        family: $family
+        taxonomy: $taxonomy
+        longitude_dd: $longitude_dd
+        latitude_dd: $latitude_dd
+        altitude_min_m: $altitude_min_m
+        altitude_max_m: $altitude_max_m
+        altitude_mean_m: $altitude_mean_m
+        location_region: $location_region
+        location_specific: $location_specific
+        habitat_type: $habitat_type
+        observation_month_begin: $observation_month_begin
+        observation_month_end: $observation_month_end
+        observation_year_begin: $observation_year_begin
+        observation_year_end: $observation_year_end
+        observation_season: $observation_season
+        analysis_number: $analysis_number
+        prey_kingdom: $prey_kingdom
+        prey_phylum: $prey_phylum
+        prey_class: $prey_class
+        prey_order: $prey_order
+        prey_suborder: $prey_suborder
+        prey_family: $prey_family
+        prey_genus: $prey_genus
+        prey_scientific_name: $prey_scientific_name
+        inclusive_prey_taxon: $inclusive_prey_taxon
+        prey_name_ITIS_ID: $prey_name_ITIS_ID
+        prey_name_status: $prey_name_status
+        prey_stage: $prey_stage
+        prey_part: $prey_part
+        prey_common_name: $prey_common_name
+        fraction_diet: $fraction_diet
+        diet_type: $diet_type
+        item_sample_size: $item_sample_size
+        bird_sample_size: $bird_sample_size
+        sites: $sites
+        study_type: $study_type
+        notes: $notes
+        entered_by: $entered_by
+        source: $source
+        doi: $string
+        sex: $string
+        age_class: $string
+        within_study_data_source: $string
+        table_fig_number: $string
+        title: $string
+        lastname_author: $string
+        year: $number
+        journal: $string
+        total_percent_diet: $number
+        unique_id: $number
+    )
+}
+`;
+
+const DENY_PENDING_DIET = gql`
+mutation DenyPendingDiet(
+    $common_name: string!
+    $scientific_name: string
+    $subspecies: string
+    $family: string
+    $taxonomy: string
+    $longitude_dd: string
+    $latitude_dd: string
+    $altitude_min_m: string
+    $altitude_max_m: string
+    $altitude_mean_m: string
+    $location_region: string!
+    $location_specific: string
+    $habitat_type: string
+    $observation_month_begin: number
+    $observation_month_end: number
+    $observation_year_begin: number
+    $observation_year_end: number
+    $observation_season: string
+    $analysis_number: string
+    $prey_kingdom: string!
+    $prey_phylum: string
+    $prey_class: string
+    $prey_order: string
+    $prey_suborder: string
+    $prey_family: string
+    $prey_genus: string
+    $prey_scientific_name: string
+    $inclusive_prey_taxon: string
+    $prey_name_ITIS_ID: string
+    $prey_name_status: string
+    $prey_stage: string
+    $prey_part: string
+    $prey_common_name: string
+    $fraction_diet: string
+    $diet_type: string!
+    $item_sample_size: number
+    $bird_sample_size: number
+    $sites: string
+    $study_type: string
+    $notes: string
+    $entered_by: string
+    $source: string!
+    $doi: string
+    $sex: string
+    $age_class: string
+    $within_study_data_source: string
+    $table_fig_number: string
+    $title: string
+    $lastname_author: string
+    $year: number
+    $journal: string
+    $total_percent_diet: number
+    $unique_id: number
+    ) {
+    denyPendingDiet(           
+        common_name: $common_name
+        scientific_name: $scientific_name
+        subspecies: $subspecies
+        family: $family
+        taxonomy: $taxonomy
+        longitude_dd: $longitude_dd
+        latitude_dd: $latitude_dd
+        altitude_min_m: $altitude_min_m
+        altitude_max_m: $altitude_max_m
+        altitude_mean_m: $altitude_mean_m
+        location_region: $location_region
+        location_specific: $location_specific
+        habitat_type: $habitat_type
+        observation_month_begin: $observation_month_begin
+        observation_month_end: $observation_month_end
+        observation_year_begin: $observation_year_begin
+        observation_year_end: $observation_year_end
+        observation_season: $observation_season
+        analysis_number: $analysis_number
+        prey_kingdom: $prey_kingdom
+        prey_phylum: $prey_phylum
+        prey_class: $prey_class
+        prey_order: $prey_order
+        prey_suborder: $prey_suborder
+        prey_family: $prey_family
+        prey_genus: $prey_genus
+        prey_scientific_name: $prey_scientific_name
+        inclusive_prey_taxon: $inclusive_prey_taxon
+        prey_name_ITIS_ID: $prey_name_ITIS_ID
+        prey_name_status: $prey_name_status
+        prey_stage: $prey_stage
+        prey_part: $prey_part
+        prey_common_name: $prey_common_name
+        fraction_diet: $fraction_diet
+        diet_type: $diet_type
+        item_sample_size: $item_sample_size
+        bird_sample_size: $bird_sample_size
+        sites: $sites
+        study_type: $study_type
+        notes: $notes
+        entered_by: $entered_by
+        source: $source
+        doi: $string
+        sex: $string
+        age_class: $string
+        within_study_data_source: $string
+        table_fig_number: $string
+        title: $string
+        lastname_author: $string
+        year: $number
+        journal: $string
+        total_percent_diet: $number
+        unique_id: $number
+    )
 }
 `;
 
 export {
     CREATE_PENDING_DIET_SKELETON,
+    CREATE_PENDING_DIET_TOTAL,
+    APPROVE_PENDING_DIET,
+    DENY_PENDING_DIET,
     CREATE_USER,
     RESET_PASSWORD
 }
