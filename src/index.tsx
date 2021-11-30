1f9e39a3709138993ff00f4ffebd444630654fd7
Pich Him <pichhim@live.unc.edu>
2021-11-29 22:44:34 -0500

    Final change to staging before merging to master -- change backend uri to prod backend, remove console.logs

--- src/index.tsx
+++ src/index.tsx
@@ -9,26 +9,26 @@ import { onError } from "@apollo/client/link/error";
 
 // Add errorLink for more detail on errors generated on network and graphql level
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
-    );
+  // if (graphQLErrors)
+  //   graphQLErrors.forEach(({ message, locations, path }) =>
+  //     console.log(
+  //       `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+  //     ),
+  //   );
 
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  // if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
 const httpLink = new HttpLink({
-  uri: "http://dev-backend-pichhim.apps.cloudapps.unc.edu/graphql"
+  // Backend uri for dev (CloudApps namespace: pichhim)
+  // uri: "http://dev-backend-pichhim.apps.cloudapps.unc.edu/graphql"
+  // Backend uri for prod
+  uri: "https://back-end-dept-dietdatabase.cloudapps.unc.edu/graphql",
 });
 
 const client = new ApolloClient({
   cache,
   link: from([errorLink, httpLink]),
-  // TODO: When finalizing changes in prod, can just uncomment prod DB link (line 13)
-  // uri: "http://dev-backend-pichhim.apps.cloudapps.unc.edu/graphql",
-  // uri: "https://back-end-dept-dietdatabase.cloudapps.unc.edu/graphql",
 });
 
 ReactDOM.render(
